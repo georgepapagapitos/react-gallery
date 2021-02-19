f01fe39169d4e942503522a93a2496d349828a02
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-18 18:09:29 -0600

    finished GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  let [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    getGallery();
+  }, []);
+
+  const getGallery = () => {
+    axios
+      .get('/gallery')
+      .then((response) => {
+        console.log('in getGallery', response.data);
+        setGallery(response.data);
+      })
+      .catch((error) => {
+        console.log('error in getGallery', error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
