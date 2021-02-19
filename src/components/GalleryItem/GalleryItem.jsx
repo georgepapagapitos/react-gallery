d3bcda3709decab45bfbcb7e3f408925f3c1c0bb
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-18 20:40:00 -0600

    style + formatting

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,16 +1,11 @@
+import './GalleryItem.css';
+
 function GalleryItem({ item, addLike }) {
   return (
     <div className="responsive">
       <div className="gallery">
-        <a target="_blank" href={item.path}>
-          <img
-            src={item.path}
-            alt={item.description}
-            width="600"
-            height="400"
-          />
-        </a>
-        <div className="likes">
+        <img src={item.path} alt={item.description} />
+        <div className="like-section">
           <button onClick={addLike} data-id={item.id}>
             ❤️
           </button>
