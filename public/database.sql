7fa4175848e553e7c8a8009711e6909956cd2fce
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-21 11:39:54 -0600

    added database

---
+++ public/database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE images (
+	id SERIAL PRIMARY KEY,
+	path VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	likes INTEGER DEFAULT 0
+);
+
+INSERT INTO images (path, description)
+VALUES ('https://picsum.photos/150', 'Random photo'),
+('https://picsum.photos/150', 'Random photo'),
+('https://picsum.photos/150', 'Random photo'),
+('https://picsum.photos/150', 'Random photo'),
+('https://picsum.photos/150', 'Random photo'),
+('https://picsum.photos/150', 'Random photo'); 
+
+
+
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const imageId = req.params.id;
+    const queryText = `UPDATE images SET likes=likes+1 WHERE id=$1`
+    pool.query(queryText, [imageId]).then(result => {
+      res.sendStatus(200)
+    }).catch(error => {
+      console.log('error in PUT', error);
+    })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+  const queryText = ` SELECT * FROM "images" ORDER BY id ASC`
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  }).catch(error => {
+    console.log('error in GET', error);
+    res.sendStatus(500);
+  })
+    
 }); // END GET Route
 
 module.exports = router;
